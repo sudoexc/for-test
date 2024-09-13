1b227ab1c231114203dbff5dd0b1bde37e59ed2e
Joe <119156692+sudoexc@users.noreply.github.com>
2024-09-13 07:53:29 -0700

    Add files via upload

---
+++ js/qa.js
@@ -0,0 +1,14 @@
+const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+        question.addEventListener('click', () => {
+            item.classList.toggle('active');
+            const answer = item.querySelector('.faq-answer');
+            if (item.classList.contains('active')) {
+                answer.style.maxHeight = answer.scrollHeight + "px";
+            } else {
+                answer.style.maxHeight = 0;
+            }
+        });
+    });
